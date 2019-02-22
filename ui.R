cd085781fd7986e4f5a43ef7f894f65d9b58768c
filippo.abbondanza <33876104+fabbondanza@users.noreply.github.com>
2019-02-22 21:00:08 +0000

    Delete ui.R

--- ui.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-
-
-fluidPage(
-  dateInput(inputId = 'date1', label = 'Date of your first scan'),
-  numericInput(inputId = 'vol1', label = 'Volume (mm3) of your first scan', min = 0, max = 50)
-  dateInput(inputId = 'date2', label = 'Date of your second scan')
-)
-
