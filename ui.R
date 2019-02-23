5e5bb12f1c26a18780405b02226a182def2ba0f9
Filippo Abbondanza <fa36@st-andrews.ac.uk>
2019-02-23 13:07:00 +0000

    Newer

--- ui.R
+++ ui.R
@@ -1,10 +1,24 @@
 library(shiny)
 
 
-fluidPage(
-  dateInput(inputId = 'date1', label = 'Date of your first scan'),
-  numericInput(inputId = 'diam1', label = 'Diameter (mm3) of your first scan', min = 0, max = 50),
-  dateInput(inputId = 'date2', label = 'Date of your second scan'),
-  numericInput(inputId = 'diam2', label = 'Diameter (mm3) of your first scan', min = 0, max = 50)
+shinyUI(fluidPage(
+  titlePanel('Calcualtion VDT'),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sidebarnput(
+          dateInput(inputId = 'date1', label = 'Date of your first scan'),
+          numericInput(inputId = 'diam1', label = 'Diameter (cm) of your first scan', min = 0, max = 50),
+          dateInput(inputId = 'date2', label = 'Date of your second scan'),
+          numericInput(inputId = 'diam2', label = 'Diameter (cm) of your first scan', min = 0, max = 50),
+          output$vdt
+          out
   # checkboxInput(inputId = '')
-)
+  
+    ))),
+  
+  mainPanel(
+    plotOutput('vdt_plot')
+  )
+  
+))
