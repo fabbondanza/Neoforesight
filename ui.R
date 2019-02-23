ee8a48cf7e6d085350b485ca934e3bdaf2401a9b
Ezgi Tanriver Ayder <ezgitanriver@gmail.com>
2019-02-23 20:02:11 +0000

    -m

--- ui.R
+++ ui.R
@@ -6,13 +6,12 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      sidebarnput(
+      sliderInput(
           dateInput(inputId = 'date1', label = 'Date of your first scan'),
           numericInput(inputId = 'diam1', label = 'Diameter (cm) of your first scan', min = 0, max = 50),
           dateInput(inputId = 'date2', label = 'Date of your second scan'),
           numericInput(inputId = 'diam2', label = 'Diameter (cm) of your first scan', min = 0, max = 50),
-          output$vdt
-          out
+    
   # checkboxInput(inputId = '')
   
     ))),
