1620b4dfe927a6ab35d6f02ce3b6377d47297a18
Filippo Abbondanza <fa36@st-andrews.ac.uk>
2019-02-24 00:07:45 +0000

    Latest version

--- ui.R
+++ ui.R
@@ -2,25 +2,30 @@ library(shiny)
 
 
 shinyUI(fluidPage(
-  titlePanel('Calcultion VDT'),
+  titlePanel('Neo Trendz: Calculator for VDT and Time to Next Tumor Stage'),
   
   sidebarLayout(
     sidebarPanel(
       wellPanel(
-          dateInput(inputId = 'date1', label = 'Date of your first scan'),
-          numericInput(inputId = 'diam1', label = 'Diameter (cm) of your first scan',
-                       value = NULL, min = 0, max = 50, step = 0.01),
-          dateInput(inputId = 'date2', label = 'Date of your second scan'),
-          numericInput(inputId = 'diam2', label = 'Diameter (cm) of your first scan',
-                       value = NULL, min = 0, max = 50, step = 0.01),
-          submitButton(text = 'Calculate VDT')),
-          textOutput('values1'),
+        dateInput(inputId = 'date1', label = 'Date of the first scan'),
+        numericInput(inputId = 'diam1', label = 'Diameter (cm) of the first scan',
+                     value = NULL, min = 0, max = 50, step = 0.01),
+        dateInput(inputId = 'date2', label = 'Date of the second scan'),
+        numericInput(inputId = 'diam2', label = 'Diameter (cm) of the first scan',
+                     value = NULL, min = 0, max = 50, step = 0.01),
+        submitButton(text = 'Calculate')),
+        textOutput('values1'),
           br(),
           textOutput('values2'),
           br(),
-          textOutput('values3')),
+          textOutput('values3'),
+          br(),
+          textOutput('values4'),
+          br(),
+          textOutput('values5'),
+          br(),
+          textOutput('values6')),
   
-
   mainPanel(
     plotOutput('plot')
   ))
